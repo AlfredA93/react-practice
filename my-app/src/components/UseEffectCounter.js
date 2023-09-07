f4a3a228f20a75b903182308bfe7a695fcae79cd
Alfred Ayre <alfred.w.ayre@gmail.com>
2023-09-07 12:51:53 +0200

    Add conditional useEffect

--- my-app/src/components/UseEffectCounter.js
+++ my-app/src/components/UseEffectCounter.js
@@ -1,10 +1,20 @@
 import React, { useState, useEffect } from 'react'
 
 function UseEffectCounter() {
+
   const [count, setCount] = useState(0);
+  const [count2, setCount2] = useState(0);
+
   useEffect(() => {
+    console.log('count 1 effect')
     document.title = count;
-  });
+  }, [count]); // Specify which parameter to be updated
+
+  useEffect(() => {
+    console.log('count 2 effect')
+    document.title = count2;
+  }, [count2]); // Specify which parameter to be updated
+
   return (
     <div>
       <button
@@ -12,6 +22,12 @@ function UseEffectCounter() {
         () => setCount(count => count + 1)
       }>
           Increment Count ({count})
+      </button>
+      <button
+      onClick={
+        () => setCount2(count2 => count2 + 1)
+      }>
+          Increment Count ({count2})
       </button>
     </div>
   )
